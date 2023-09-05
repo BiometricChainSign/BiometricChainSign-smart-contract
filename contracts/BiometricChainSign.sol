a2f94d03a1e517e4255971148971664dc3a8f4e4
constantini21 <gustavoconstantinimattos.21@gmail.com>
2023-09-04 21:01:40 -0300

    feat: Implement signature add

--- contracts/BiometricChainSign.sol
+++ contracts/BiometricChainSign.sol
@@ -3,26 +3,51 @@ pragma solidity ^0.8.0;
 
 contract BiometricChainSign {
   mapping(address => Signatory) public signatories;
-  Signature[] public signatures;
+
+  // documentHash => signatoryAddress[]
+  mapping(string => address[]) public signatures;
 
   function createSignatory(string memory _cid) public {
     Signatory memory signatory = Signatory(msg.sender, _cid);
     signatories[msg.sender] = signatory;
   }
 
-  function getSignatory(
-    address _address
-  ) public view returns (Signatory memory) {
+  function getSignatory(address _address) public view returns (Signatory memory) {
     return signatories[_address];
+  }
+
+  function addSignature(string memory _documentHash) public {
+    Signatory memory signatory = signatories[msg.sender];
+
+    require(bytes(signatory.cid).length != 0, "Signatory not found");
+
+    if (!arrayHasAddress(msg.sender, signatures[_documentHash])) {
+      signatures[_documentHash].push(msg.sender);
+    }
+  }
+
+  function getSignatureSignatories(
+    string memory _documentHash
+  ) public view returns (address[] memory) {
+    return signatures[_documentHash];
+  }
+
+  function arrayHasAddress(
+    address searchAddress,
+    address[] memory addressArray
+  ) private pure returns (bool) {
+    for (uint256 i = 0; i < addressArray.length; i++) {
+      if (addressArray[i] == searchAddress) {
+        // Address found, return true
+        return true;
+      }
+    }
+    // Address not found, return false
+    return false;
   }
 
   struct Signatory {
     address publicAddress;
     string cid;
-  }
-
-  struct Signature {
-    Signatory[] signatories;
-    string documentHash;
   }
 }
--- test/BiometricChainSign.ts
+++ test/BiometricChainSign.ts
@@ -1,5 +1,6 @@
 import { expect } from 'chai'
 import { ethers } from 'hardhat'
+import crypto from 'crypto'
 
 describe('BiometricChainSign', () => {
   it('should create a signatory', async () => {
@@ -11,5 +12,18 @@ describe('BiometricChainSign', () => {
     const signatory = await contract.getSignatory(signer.address)
 
     expect(signatory).to.deep.equal([signer.address, cid])
+  })
+
+  it('should add a signature', async () => {
+    const contract = await ethers.deployContract('BiometricChainSign')
+    const [signer] = await ethers.getSigners()
+
+    const cid = '123456789abcdef'
+    await contract.createSignatory(cid)
+    const documentHash = crypto.randomBytes(32).toString('hex')
+    await contract.addSignature(documentHash)
+    const signatories = await contract.getSignatureSignatories(documentHash)
+
+    expect(signatories).to.deep.equal([signer.address])
   })
 })
